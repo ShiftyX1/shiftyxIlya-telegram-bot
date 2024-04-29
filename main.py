c348881bbe25e34585dd51f4cf203765651d80d9
ShiftyX1 <143071695+ShiftyX1@users.noreply.github.com>
2024-04-29 12:17:02 +0300

    commit new

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from aiogram import Dispatcher, Bot
-from aiogram.filters import CommandStart
+from aiogram import Dispatcher, Bot, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from config import settings
 
 import asyncio
+import logging
 
 bot = Bot(token=settings.TOKEN)
 dp = Dispatcher()
@@ -13,10 +14,22 @@ dp = Dispatcher()
 async def cmd_start(message: Message):
     await message.answer("Hello!")
 
+@dp.message(Command("help"))
+async def cmd_help(message: Message):
+    await message.answer("Это комманда /help")
+
+@dp.message(F.text == 'Как дела?')
+async def how_are_you(message: Message):
+    await message.answer("Все хорошо!")
+
 
 async def main():
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Выход")
