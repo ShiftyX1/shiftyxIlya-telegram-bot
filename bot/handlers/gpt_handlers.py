8a612049bda1c7e8bbefd10213975066460dc53d
ShiftyX1 <143071695+ShiftyX1@users.noreply.github.com>
2024-05-01 01:17:57 +0300

    Добавлена обработка стикеров гифок и прочих content_types при общении с GPT, а также проверка очистки истории общения с GPT

--- bot/handlers/gpt_handlers.py
+++ bot/handlers/gpt_handlers.py
@@ -1,6 +1,6 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 from models.gpt_models import History
 from tortoise.exceptions import DoesNotExist
 
@@ -11,7 +11,7 @@ import json
 
 
 gpt_router = Router()
-
+gpt_answers_if_not_text = ["Упс! По всей видимости вы отправили картинку/гифку/стикер/документ и т.д, к сожалению, на данном этапе разработки я умею обрабатывать только текст, но в скором времени это планируется исправить!"]
 
 
 def history_to_json(history_dict: dict) -> str:
@@ -34,6 +34,11 @@ async def process_clear_command(message: Message):
     user_id = message.from_user.id
     try:
         history = await History.get(user_id=user_id)
+        
+        if history.historyjson == []:
+            await message.reply("По всей видимости ранее вы уже очищали историю диалога, очистка не требуется")
+            return None
+        
         history.historyjson = history_to_json([])
         await history.save()
         await message.reply("История диалога очищена.")
@@ -43,42 +48,45 @@ async def process_clear_command(message: Message):
 # Обработчик для каждого нового сообщения к GPT
 @gpt_router.business_message()
 async def gpt_response(message: Message):
-    user_id = message.from_user.id
-    user_input = message.text
+    if message.content_type == ContentType.TEXT:
+        user_id = message.from_user.id
+        user_input = message.text
 
-    historydb_query = await History.get_or_create(user_id=str(user_id))
+        historydb_query = await History.get_or_create(user_id=str(user_id))
 
-    if historydb_query[0].historyjson == None:
-        historydb_query[0].historyjson = history_to_json([])
-        await historydb_query[0].save()
+        if historydb_query[0].historyjson == None:
+            historydb_query[0].historyjson = history_to_json([])
+            await historydb_query[0].save()
 
-    conversation_history = await History.get(user_id=str(user_id))
-    chat_history = conversation_history.historyjson
-    chat_history.append({"role": "user", "content": user_input})
-    
-    # conversation_history[user_id] = trim_history(conversation_history[user_id])
-    #chat_history = conversation_history[user_id]
+        conversation_history = await History.get(user_id=str(user_id))
+        chat_history = conversation_history.historyjson
+        chat_history.append({"role": "user", "content": user_input})
 
-    providers = [g4f.Provider.Feedough, g4f.Provider.FreeGpt]
+        # conversation_history[user_id] = trim_history(conversation_history[user_id])
+        #chat_history = conversation_history[user_id]
 
-    try:
-        response = await g4f.ChatCompletion.create_async(
-            model=g4f.models.default,
-            messages=chat_history,
-            provider=providers[random.randint(0, 1)]
-        )
-        chat_gpt_response = response
-    except Exception as e:
-        print(f"{g4f.Provider.GeekGpt.__name__}:", e)
-        chat_gpt_response = "Извините, произошла ошибка."
+        providers = [g4f.Provider.Feedough]
 
-    chat_history.append({"role": "assistant", "content": chat_gpt_response})
-    conversation_history.historyjson = json.dumps(chat_history)
-    await conversation_history.save()
-    
-    #print(conversation_history.historyjson.encode(encoding="utf-8"))
-    #length = sum(len(message["content"]) for message in conversation_history[user_id])
-    #print(length)
+        try:
+            response = await g4f.ChatCompletion.create_async(
+                model=g4f.models.default,
+                messages=chat_history,
+                provider=providers[0]
+            )
+            chat_gpt_response = response
+        except Exception as e:
+            print(f"{g4f.Provider.GeekGpt.__name__}:", e)
+            chat_gpt_response = "Извините, произошла ошибка."
 
-    await message.answer(f"{chat_gpt_response}")
+        chat_history.append({"role": "assistant", "content": chat_gpt_response})
+        conversation_history.historyjson = json.dumps(chat_history)
+        await conversation_history.save()
+
+        #print(conversation_history.historyjson.encode(encoding="utf-8"))
+        #length = sum(len(message["content"]) for message in conversation_history[user_id])
+        #print(length)
+
+        await message.answer(f"{chat_gpt_response}")
+    else:
+        await message.answer(gpt_answers_if_not_text[0])
 
